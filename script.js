413ccd4d1ed7f2a3189a59d57c1704c92fb96de1
Kivi Corn <magiciankivi@gmail.com>
2021-04-01 00:21:53 +0300

    start mobile first bg home pg

---
+++ script.js
@@ -0,0 +1,18 @@
+const adjEl = document.getElementById('adj');
+
+const adjList = ['motivated', 'enthusiastic', 'creative', 'dedicated', 'team-player'];
+
+let displayIndex = 0;
+let delay = 1000;
+
+// setInterval(() => {
+//     if (adjEl) {
+//         adjEl.innerText = adjList[displayIndex];
+//     }
+
+//     displayIndex ++;
+
+//     if (displayIndex >= adjList.length) {
+//         displayIndex = 0;
+//     }
+// }, delay);
