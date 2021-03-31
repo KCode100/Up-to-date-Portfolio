94b7581a4ce0ff23e0bae9d8309cbfd9ffa8e2ff
Kivi Corn <magiciankivi@gmail.com>
2021-04-01 00:26:29 +0300

    changing txt feature

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const adjEl = document.getElementById('adj');
 
-const adjList = ['motivated', 'enthusiastic', 'creative', 'dedicated', 'team-player'];
+const adjList = ['motivated', 'enthusiastic', 'creative', 'dedicated', 'devoted'];
 
 let displayIndex = 0;
-let delay = 1000;
+let delay = 250;
 
-// setInterval(() => {
-//     if (adjEl) {
-//         adjEl.innerText = adjList[displayIndex];
-//     }
+setInterval(() => {
+    if (adjEl) {
+        adjEl.innerText = adjList[displayIndex];
+    }
 
-//     displayIndex ++;
+    displayIndex ++;
 
-//     if (displayIndex >= adjList.length) {
-//         displayIndex = 0;
-//     }
-// }, delay);
+    if (displayIndex >= adjList.length) {
+        displayIndex = 0;
+    }
+}, delay);
