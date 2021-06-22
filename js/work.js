bc285bf4c4e3bcb7cda8824d22a0df25872deefc
Kivi Corn <magiciankivi@gmail.com>
2021-06-22 09:09:55 +0300

    add quiz to portfolio

--- js/work.js
+++ js/work.js
@@ -5,67 +5,74 @@ let currentList = [];
 // NEW PROJECT? JUST ADD AN OBJECT IN THE ARRAY BELOW!
 let work = [
     {
-        name: 'This project',
+        name: 'ByKivi.com (WIP)',
         type: ['website', 'recent'],
         img: './img/mockup/this-project-full.jpg',
         link: 'http://bykivi.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
-    },
-    {
-        name: 'Kriah.com',
-        type: ['website'],
-        img: './img/mockup/kriah-full.jpg',
-        link: 'https://kriah.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
-    },
-    {
-        name: 'Landing page (Hebrew)',
-        type: ['website', 'recent'],
-        img: './img/mockup/hebrew-full.jpg',
-        link: 'https://kriah.com/zobin-method',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
-    },
-    {
-        name: 'Wedding Countdown',
-        type: ['app', 'recent'],
-        img: './img/mockup/countdown-full.jpg',
-        link: 'http://bykivi.com/wedding-countdown',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        description: 'A portfolio site for clients and employers to read about me and to view previous work. Resonsive site. Modern design. Vanilla JavaScript.'
     },
     {
         name: 'Landing page',
         type: ['website', 'recent'],
         img: './img/mockup/landing-pg-full.jpg',
         link: 'https://kriah.com/hebrew-course',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        description: 'A landing page for a leading educational institution. Responsive site. Modern Design. SEO. A/B testing. Facebook ads.'
+    },
+    {
+        name: 'Kriah.com',
+        type: ['website'],
+        img: './img/mockup/kriah-full.jpg',
+        link: 'https://kriah.com',
+        description: "One of the best rated reading specialist institutions in the country. This site was transformed from a template design into a bespoke, 20 page responsive website. It includes a login system for users to access resources and FAQ's."
+    },
+    {
+        name: 'Landing page (Hebrew)',
+        type: ['website', 'recent'],
+        img: './img/mockup/hebrew-full.jpg',
+        link: 'https://kriah.com/zobin-method',
+        description: 'A landing page in Hebrew for a leading educational institution. Responsive site. Modern Design. SEO. A/B testing. Facebook ads.'
+    },
+    {
+        name: 'Wedding Countdown',
+        type: ['app', 'recent'],
+        img: './img/mockup/countdown-full.jpg',
+        link: 'http://bykivi.com/wedding-countdown',
+        description: 'A personalized countdown clock to your wedding day! It saves high scores to your device using the LocalStorage API'
     },
     {
         name: 'Memory Game (24hr challenge)',
         type: ['game', 'recent'],
         img: './img/mockup/memory-game-full.jpg',
         link: 'http://bykivi.com/memory-game',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        description: "Challenge: Build a memory game using vanilla JavaScript within 24 hours. I created an interactive 'Simon Says' game. It saves high Scores, includes speed and sound settings, localStorage, and it's Mobile Responsive."
     },
     {
         name: 'Portfolio',
         type: ['website'],
         img: './img/mockup/portfolio-full.jpg',
-        link: 'http://bykivi.com',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        link: 'http://bykivi.com/portfolio',
+        description: 'An alternative portfolio website to showcase my work and skills to clients and employers.'
     },
     {
         name: 'Classic Pong Game',
         type: ['game'],
         img: './img/mockup/pong-full.jpg',
         link: 'http://bykivi.com/pong',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        description: 'One of the first computer games ever created!'
     },
     {
         name: 'Gematriah Calculator',
         type: ['app'],
         img: './img/mockup/gematriah-full.jpg',
         link: 'http://bykivi.com/gematriah',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In eum repellendus facilis veritatis perspiciatis quidem officiis ut ipsa ducimus vitae quo placeat ipsam.'
+        description: 'Calculate the numerical value of Hebrew letters and words.'
+    },
+    {
+        name: 'Quiz Game [WIP]',
+        type: ['game'],
+        img: './img/mockup/quiz.jpg',
+        link: 'http://bykivi.com/quiz',
+        description: 'Challenge yourself with a Trivia Quiz.'
     },
 ];
 
